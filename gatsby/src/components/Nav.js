cc38bdad6d82c81e5abc341c0a61bbeaefd15f46
rlycrvns <rileycravens@MU-TH-UR.local>
2020-10-27 16:48:01 -0700

    add: nav component

---
+++ gatsby/src/components/Nav.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Link } from 'gatsby';
+
+export default function Nav() {
+  return (
+    <div>
+      <ul>
+        <li>
+          <Link />
+        </li>
+        <li>
+          <Link />
+        </li>
+        <li>
+          <Link />
+        </li>
+        <li>
+          <Link />
+        </li>
+        <li>
+          <Link />
+        </li>
+      </ul>
+    </div>
+  );
+}
