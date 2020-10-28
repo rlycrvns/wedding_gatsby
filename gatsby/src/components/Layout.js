aaad866faa960e1aa6df8c440ba28d0f778d11ff
rlycrvns <rileycravens@MU-TH-UR.local>
2020-10-28 13:16:51 -0700

    add: layout component

---
+++ gatsby/src/components/Layout.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import styled from 'styled-components';
+import Footer from './Footer';
+import Nav from './Nav';
+import 'normalize.css';
+import GlobalStyles from '../styles/GlobalStyles';
+import Typography from '../styles/Typography';
+
+export default function Layout({ children }) {
+  return (
+    <>
+      <GlobalStyles />
+      <Typography />
+      <Nav />
+      {children}
+      <Footer />
+    </>
+  );
+}
