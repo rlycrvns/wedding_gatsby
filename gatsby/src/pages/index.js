a0826c04c3b8afa8849382c66108a1a32a9399ca
rlycrvns <rileycravens@MU-TH-UR.local>
2020-10-31 19:59:11 -0700

    add: photo query

--- gatsby/src/pages/index.js
+++ gatsby/src/pages/index.js
@@ -1,13 +1,33 @@
+import { graphql } from 'gatsby';
 import React from 'react';
+import Collage from '../components/Collage';
 import SEO from '../components/SEO';
 
-export default function HomePage() {
+export default function HomePage({ data }) {
+  const photos = data.photos.nodes;
   return (
     <>
       <SEO />
       <div>
+        <Collage photos={photos} />
         <h1>Home!</h1>
       </div>
     </>
   );
 }
+
+export const query = graphql`
+  query PhotoQuery {
+    photos: allSanityPhotos {
+      nodes {
+        image {
+          asset {
+            fluid(maxWidth: 800) {
+              ...GatsbySanityImageFluid
+            }
+          }
+        }
+      }
+    }
+  }
+`;
