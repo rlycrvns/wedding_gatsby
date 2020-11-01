1b14e038c5e1d1e823e9f064ce9d30d32ae4c79e
rlycrvns <rileycravens@MU-TH-UR.local>
2020-10-31 19:56:49 -0700

    add: collage comp

--- gatsby/src/components/Collage.js
+++ gatsby/src/components/Collage.js
@@ -1,5 +1,22 @@
 import React from 'react';
+import Img from 'gatsby-image';
+import styled from 'styled-components';
 
-export default function Collage() {
-  return <div />;
+const CollageStyles = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr;
+`;
+
+function SinglePhoto({ photo }) {
+  return <Img fluid={photo.image.asset.fluid} alt={photo.id} />;
+}
+
+export default function Collage({ photos }) {
+  return (
+    <CollageStyles className="photo-collage">
+      {photos.map((photo) => (
+        <SinglePhoto key={photo.id} photo={photo} />
+      ))}
+    </CollageStyles>
+  );
 }
