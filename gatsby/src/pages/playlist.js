558a3d25ec5eeefa3e4ff242588eaabb99ce860c
rlycrvns <rileycravens@MU-TH-UR.local>
2020-11-10 17:04:33 -0800

    add: content and style

--- gatsby/src/pages/playlist.js
+++ gatsby/src/pages/playlist.js
@@ -1,10 +1,25 @@
 import React from 'react';
+import styled from 'styled-components';
+import H1 from '../components/H1';
 import SpotifyPlayer from '../components/SpotifyPlayer';
+
+const PlaylistStyles = styled.div``;
 
 export default function PlaylistPage() {
   return (
     <div>
-      <h1>Playlist!</h1>
+      <H1 title="Playlist" />
+      <p>Help us build the perfect playlist!</p>
+      <p>
+        Add songs to our official
+        <a
+          href="https://open.spotify.com/playlist/7m7U9TvjNUJgC0dam4RU1e?si=_8euDRYAQOmxJ18pWqzcxA"
+          rel="noreferrer"
+          target="_blank"
+        >
+          Spotify Playlist
+        </a>
+      </p>
       <SpotifyPlayer />
     </div>
   );
