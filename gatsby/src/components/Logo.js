d7fb44e1bb02fa4fbfe3ffc137c879845a3fad3b
rlycrvns <rileycravens@MU-TH-UR.local>
2020-11-27 13:29:57 -0800

    add: logo animation

--- gatsby/src/components/Logo.js
+++ gatsby/src/components/Logo.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import { gsap } from 'gsap';
+import React, { useEffect } from 'react';
 import styled from 'styled-components';
 import LogoSVG from '../assets/images/logo.svg';
 
@@ -14,6 +15,35 @@ const LogoStyles = styled.div`
 `;
 
 export default function Logo() {
+  const t1 = gsap.timeline({ delay: 0.5 });
+
+  useEffect(() => {
+    t1.fromTo(
+      '.logo-bg',
+      { scale: 0, opacity: 0, transformOrigin: 'center' },
+      { duration: 0.75, scale: 1, opacity: 1, ease: 'back.out(1.4)' }
+    )
+      .fromTo(
+        '.logo-jb',
+        { scaleY: 0, opacity: 0, transformOrigin: 'top' },
+        { scaleY: 1, opacity: 1, duration: 1, ease: 'bounce.out' }
+      )
+      .fromTo(
+        '.logo-rc',
+        { scaleY: 0, opacity: 0, transformOrigin: 'top' },
+        { scaleY: 1, opacity: 1, duration: 1, ease: 'bounce.out' }
+      )
+      .fromTo(
+        '.logo-border',
+        { scale: 0, transformOrigin: 'top left' },
+        { scale: 1, duration: 0.5, ease: 'bounce.out' }
+      )
+      .fromTo(
+        '.logo-plus',
+        { scale: 0, opacity: 0, transformOrigin: 'center' },
+        { scale: 1, opacity: 1, ease: 'back.out(3)' }
+      );
+  }, []);
   return (
     <LogoStyles className="logo">
       <LogoSVG />
