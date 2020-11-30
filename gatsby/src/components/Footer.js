6c7552c73e91aaf63aee21e4c07c5b945252f522
rlycrvns <rileycravens@MU-TH-UR.local>
2020-11-29 17:11:08 -0800

    update: body fonts

--- gatsby/src/components/Footer.js
+++ gatsby/src/components/Footer.js
@@ -6,7 +6,7 @@ const FooterStyles = styled.footer`
   margin-bottom: 2rem;
   p {
     font-size: 1.5rem;
-    font-family: 'BodySemi';
+    font-family: 'BodyBold';
   }
   a {
     color: var(--green);
