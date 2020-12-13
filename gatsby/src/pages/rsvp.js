2d547f6e4ce7b0699ebfcf29d5b3d406d5b33330
rlycrvns <rileycravens@MU-TH-UR.local>
2020-12-13 15:40:10 -0800

    add: content/style

--- gatsby/src/pages/rsvp.js
+++ gatsby/src/pages/rsvp.js
@@ -1,10 +1,25 @@
 import React from 'react';
+import styled from 'styled-components';
 import H1 from '../components/H1';
+
+const RSVPStyles = styled.div`
+  p {
+    text-align: center;
+    font-family: 'BodyLight';
+    font-size: 4.5rem;
+    width: 80%;
+    margin: auto;
+    margin-top: 4rem;
+  }
+`;
 
 export default function Rsvp() {
   return (
-    <div>
+    <RSVPStyles>
       <H1 title="RSVP" />
-    </div>
+      <p>
+        We aren't quite ready for you to RSVP yet, please check back after you have received your formal invitation.
+      </p>
+    </RSVPStyles>
   );
 }
