af789355a5c85e0373031fda4ef23787aba68492
rlycrvns <rileycravens@MU-TH-UR.local>
2020-12-13 18:20:06 -0800

    add: style from story photos comp

--- gatsby/src/templates/Posts.js
+++ gatsby/src/templates/Posts.js
@@ -1,4 +1,4 @@
-import { graphql } from 'gatsby';
+import { graphql, Link } from 'gatsby';
 import React from 'react';
 import Img from 'gatsby-image';
 import styled from 'styled-components';
@@ -6,8 +6,48 @@ import SEO from '../components/SEO';
 
 const PostGrid = styled.div`
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
+
+  .photo {
+    width: 100%;
+    max-width: 1200px;
+    margin: 4rem auto;
+    background-color: var(--pink);
+  }
+  .gatsby-image-wrapper {
+    margin: 2rem 2rem 0 2rem;
+  }
+  h1 {
+    color: var(--black);
+    border-radius: 0.2rem;
+    font-family: HeadingReg;
+    font-size: clamp(3rem, 2.5vw, 5rem);
+    background-color: var(--pale-green);
+    width: 100%;
+    margin-bottom: 1.5rem;
+    transform: rotate(-2deg);
+    margin-top: -2rem;
+    padding: 0.5rem 0;
+    text-align: center;
+    @media (min-width: 800px) {
+      width: 800px;
+      margin-left: auto;
+      margin-right: auto;
+    }
+  }
+  a {
+    font-family: 'HeadingCursive';
+    font-size: 4rem;
+    color: var(--green);
+    transition: color 0.25s ease-in-out;
+    &:hover,
+    &:active,
+    &:focus {
+      color: var(--rust);
+    }
+  }
 `;
 
 export default function SinglePostPage({ data: { post } }) {
@@ -15,8 +55,11 @@ export default function SinglePostPage({ data: { post } }) {
     <>
       <SEO title="Post" image={post.image?.asset?.fluid?.src} />
       <PostGrid>
-        <Img fluid={post.image.asset.fluid} />
-        <h1>{post.name}</h1>
+        <div className="photo">
+          <Img fluid={post.image.asset.fluid} />
+          <h1>{post.caption}</h1>
+        </div>
+        <Link to="/ourStory">Back</Link>
       </PostGrid>
     </>
   );
@@ -28,7 +71,7 @@ export const query = graphql`
       id
       image {
         asset {
-          fluid(maxWidth: 800) {
+          fluid(maxWidth: 1200) {
             ...GatsbySanityImageFluid
           }
         }
