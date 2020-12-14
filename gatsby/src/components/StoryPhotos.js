bdd2ad868367144dd886dd4cee74c0331c1380a5
rlycrvns <rileycravens@MU-TH-UR.local>
2020-12-13 18:19:14 -0800

    add: link tags and style

--- gatsby/src/components/StoryPhotos.js
+++ gatsby/src/components/StoryPhotos.js
@@ -1,38 +1,7 @@
 import React from 'react';
 import Img from 'gatsby-image';
 import styled from 'styled-components';
-
-const SinglePostStyles = styled.div`
-  margin: 1rem;
-  padding: 1rem 1rem 0 1rem;
-  background-color: var(--pink);
-  @media (max-width: 799px) {
-    width: 95%;
-  }
-  .gatsby-image-wrapper {
-    border-radius: 0.5rem;
-    width: 250px;
-    @media (max-width: 799px) {
-      width: 100%;
-    }
-  }
-  p {
-    border-radius: 0.2rem;
-    font-family: HeadingReg;
-    font-size: 2rem;
-    background-color: var(--pale-green);
-    width: 100%;
-    margin-bottom: 1.5rem;
-    transform: rotate(-2deg);
-    margin-top: -2rem;
-    padding: 0.5rem 0;
-    @media (min-width: 800px) {
-      width: 240px;
-      margin-left: auto;
-      margin-right: auto;
-    }
-  }
-`;
+import { Link } from 'gatsby';
 
 const PostStyles = styled.div`
   width: 100%;
@@ -52,14 +21,54 @@ const PostStyles = styled.div`
   @media (min-width: 1500px) {
     height: 5100px;
   }
+  a {
+    /* width: 100%; */
+    margin: 1rem;
+    padding: 1rem 1rem 0 1rem;
+    background-color: var(--pink);
+    transition: all 0.25s ease-in-out;
+    &:hover,
+    &:active,
+    &:focus {
+      transform: scale(0.9);
+    }
+    @media (max-width: 799px) {
+      width: 80%;
+      margin: 1rem auto;
+    }
+  }
+  .gatsby-image-wrapper {
+    border-radius: 0.5rem;
+    width: 250px;
+    @media (max-width: 799px) {
+      width: 100%;
+    }
+  }
+  p {
+    color: var(--black);
+    border-radius: 0.2rem;
+    font-family: HeadingReg;
+    font-size: 2rem;
+    background-color: var(--pale-green);
+    width: 100%;
+    margin-bottom: 1.5rem;
+    transform: rotate(-2deg);
+    margin-top: -2rem;
+    padding: 0.5rem 0;
+    @media (min-width: 800px) {
+      width: 240px;
+      margin-left: auto;
+      margin-right: auto;
+    }
+  }
 `;
 
 function SinglePost({ post }) {
   return (
-    <SinglePostStyles>
+    <Link to={`/${post.slug.current}`}>
       <Img fluid={post.image.asset.fluid} alt="" />
       <p>{post.caption}</p>
-    </SinglePostStyles>
+    </Link>
   );
 }
 
