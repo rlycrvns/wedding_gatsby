f2fc2ae5f2e93df98a60260fe3590b142bdd14b6
rlycrvns <rlycrvns@gmail.com>
2020-12-21 16:20:59 -0800

    update: style

--- gatsby/src/pages/playlist.js
+++ gatsby/src/pages/playlist.js
@@ -10,9 +10,25 @@ const PlaylistStyles = styled.div`
     font-family: 'BodyLight';
     text-align: center;
     font-size: 2.5rem;
+    @media (max-width: 799px) {
+      max-width: 300px;
+      margin-left: auto;
+      margin-right: auto;
+    }
   }
   a {
+    display: block;
     transition: color 0.25s ease-in-out;
+    background-color: var(--pink);
+    font-size: 2.8rem;
+    width: 300px;
+    margin-top: 1rem;
+    margin-left: auto;
+    margin-right: auto;
+    padding: 1rem;
+    border: 2px solid var(--green);
+    border-radius: 2rem;
+    box-shadow: 0 20px 50px rgba(113, 136, 120, 0.7);
     &:hover,
     &:active,
     &:focus {
